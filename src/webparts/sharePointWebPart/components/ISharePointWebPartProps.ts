e454774602142b2ed2aaae4f26f186997078faa4
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-02-10 18:23:55 -0300

    first commit sharepoint with React

---
+++ src/webparts/sharePointWebPart/components/ISharePointWebPartProps.ts
@@ -0,0 +1,7 @@
+export interface ISharePointWebPartProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/sharePointWebPart/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ISharePointWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'SharePointWebPartWebPartStrings' {
+  const strings: ISharePointWebPartWebPartStrings;
+  export = strings;
+}
