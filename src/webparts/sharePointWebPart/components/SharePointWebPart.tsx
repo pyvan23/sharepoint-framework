66ec4da30839701611f6bcc28591a6d29ccc5d30
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-02-11 11:17:51 -0300

    certificate install

--- src/webparts/sharePointWebPart/components/SharePointWebPart.tsx
+++ src/webparts/sharePointWebPart/components/SharePointWebPart.tsx
@@ -23,9 +23,7 @@ export default class SharePointWebPart extends React.Component<ISharePointWebPar
         </div>
         <div>
           <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-          </p>
+        
           <h4>Learn more about SPFx development:</h4>
           <ul className={styles.links}>
             <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
